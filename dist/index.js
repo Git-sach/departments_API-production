35faf891e9cfbe872c5c59e1da5dd3ee8f0dafe2
Adrien Cheynet <gitcha@outlook.fr>
2024-01-26 12:35:48 +0100

    Add middleware CORS

--- dist/index.js
+++ dist/index.js
@@ -6,17 +6,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const departments_1 = require("./controllers/departments");
 const environement_1 = require("./environement");
+const cors = require("cors");
 const app = (0, express_1.default)();
-const morgan = require('morgan');
-const path = require('path');
-app.use(morgan('short'));
-app.use(express_1.default.static(path.join(__dirname, '../public')));
+const morgan = require("morgan");
+const path = require("path");
+app.use(morgan("short"));
+app.use(express_1.default.static(path.join(__dirname, "../public")));
+app.use(cors());
 const departmentController = new departments_1.DepartmentController();
-app.get('/departments', departmentController.departmentsList);
+app.get("/departments", departmentController.departmentsList);
 // faire avec un query param plutot
-app.get('/departments/:name', departmentController.departmentsList);
-app.get('*', (req, res) => {
-    res.json({ "test": "ok" });
+app.get("/departments/:name", departmentController.departmentsList);
+app.get("*", (req, res) => {
+    res.json({ test: "ok" });
 });
 app.listen(environement_1.PORT);
 // pour la mise en production, penser a faire npm install --production pour ne pas installer toutes les dépendances inutiles
